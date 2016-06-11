59875e13d610dfa9e51baa00b07d45170e91aead
tacigar <ig.lhound@gmail.com>
2016-06-11 11:45:38 +0900

    Fix typo

---
+++ ARC-001-B/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <fstream>
+#include <limits>
+#include <cmath>
+
+int ARRAY[] = {1, 5, 10, -1, -5, -10};
+
+int search(int from, int to) {
+    int min = std::numeric_limits<int>::max(), min_i = 0;
+    for (int i = 0; i < 6; i++) {
+        int buf = std::abs(to - from - ARRAY[i]);
+        if (buf < min) {
+            min = buf;
+            min_i = i;
+        }
+    }
+    return min_i;
+}
+
+int main(int argc, char** argv) {
+    std::ifstream ifs(argv[1]);
+    int from, to, count = 0;
+    ifs >> from >> to;
+    while (true) {
+        if (from == to) break;
+        else count++;
+        int index = search(from, to);
+        from += ARRAY[index];
+    }
+    std::cout << count << std::endl;
+}
